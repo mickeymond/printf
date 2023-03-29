6c80cecd9ab251cf3eac41d0051c4b0079e62243
MaverickXY <aneleobinnaa@gmail.com>
2023-03-28 21:55:18 -0700

    printf group project

--- _printf.c
+++ _printf.c
@@ -1,47 +1,50 @@
 #include "main.h"
+#include <limits.h>
+#include <stdio.h>
 
 /**
- * _printf - Outputs a formatted string.
- * @format: Character string to print - may contain directives.
- * Return: The number of characters printed.
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
-	va_list arg;
-	int counter = 0;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	va_start(arg, format);
-	while (*format != '\0')
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
 	{
-		if (*format == '%')
+		if (*p == '%')
 		{
-			format++;
-			if (*format == 's')
+			p++;
+			if (*p == '%')
 			{
-				_printf_string(va_arg(arg, char*), &counter);
+				count += _putchar('%');
+				continue;
 			}
-			else if (*format == 'c')
-			{
-				_putchar(va_arg(arg, int));
-				counter++;
-			}
-			else if (*format == '%')
-			{
-				_putchar(*format);
-				counter++;
-			}
-			else
-			{
-				_putchar(*format);
-				counter++;
-			}
-			format++;
-			continue;
-		}
-		_putchar(*format);
-		format++;
-		counter++;
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
 	}
-	va_end(arg);
-	return (counter);
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
+
 }
